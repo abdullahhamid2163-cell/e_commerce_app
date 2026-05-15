835b2047e54eb41e66156e14e4f022f395485135
Abdullah1Bashir <lahoreedufund201819@gmail.com>
2026-05-15 16:34:40 +0500

    project

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
-import 'package:e_commerce_app/utils/theme/theme.dart';
+import 'package:e_commerce_app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: ThemeData(),
-      darkTheme: Theming.darkTheme,
-
-      //home: ,      
-    );
-  }
 }
---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // App Basic Colors
+  static const Color prim = Color(0xFF4b68ff);
+  static const Color sec = Color(0xFFFFE248);
+  static const Color acc = Color(0xFFb0c7ff);
+
+  // Text Colors
+  static const Color textPrim = Color(0xFF333333);
+  static const Color textSec = Color(0xFF6C757D);
+  static const Color textWhite = Color(0xFFFFFFFF);
+
+  // Background Colors
+  static const Color light = Color(0xFFF6F6F6);
+  static const Color dark = Color(0xFF272727);
+  static const Color bgPrim = Color(0xFFF3F5FF);
+
+  // Button Colors
+  static const Color buttonPrim = Color(0xFF4b68ff);
+  static const Color buttonSec = Color(0xFF6C757D);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
+
+  // Border Colors
+  static const Color borderPrim = Color(0xFFD9D9D9);
+  static const Color borderSec = Color(0xFFE6E6E6);
+
+  // Error Colors
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+  static const Color warning = Color(0xFFF57C00);
+  static const Color info = Color(0xFF1976D2);
+
+  // Neutral Shades
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // Gradient Colors
+  static const Gradient linerGradient = LinearGradient(
+    begin: Alignment(0.0, 0.0),
+    end: Alignment(0.707, -0.707),
+    colors: [Color(0xffff9a9e), Color(0xfffad0c4), Color(0xfffad0c4)],
+  );
+}
