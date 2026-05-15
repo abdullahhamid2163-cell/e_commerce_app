0146a49de6468ffd5869d70b22230bc6ab094d12
Abdullah1Bashir <lahoreedufund201819@gmail.com>
2026-05-15 17:12:41 +0500

    project

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String darkAppLogo = 'assets/images/dark_logo.png';
+  static const String lightAppLogo = 'assets/images/light_logo.png';
+
+  static const String google = 'assets/images/google-icon.png';
+  static const String facebook = 'assets/images/facebook-icon.png';
+}
