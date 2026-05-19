6f1d3d0c4c7332724e7236e0bd6c48afade97232
Abdullah1Bashir <lahoreedufund201819@gmail.com>
2026-05-19 10:12:59 +0500

    project

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/home_page.dart';
 import 'package:e_commerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
-      theme: ThemeData(),
+      theme: ThemeData.light(),
       darkTheme: Theming.darkTheme,
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image(image: AssetImage('assets/images/image_search.png')),
+      ),
+    );
+  }
+}
